9b2fce63f32e9392c56bfab73e24561271245b84
Mick George <predatorie.mg@gmail.com>
2020-11-11 18:54:01 -0500

    Initial commit

---
+++ android/app/src/main/java/com/mallen/flutter_versionone/MainActivity.java
@@ -0,0 +1,6 @@
+package com.mallen.flutter_versionone;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/secrets/secrets.dart
@@ -0,0 +1,5 @@
+abstract class Secrets {
+  static String clientSecret = "ADD YOUR KEY HERE";
+  static String encryptionKey = 'ADD YOUR KEY HERE';
+  static String versionOneApiKey = 'ADD YOUR KEY HERE';
+}
