5a3a006c89eb393dbe6b438c553d88f67a045631
Mick George <predatorie.mg@gmail.com>
2020-11-11 19:13:20 -0500

    deleted .gitignore

--- lib/secrets/secrets.dart
+++
@@ -1,5 +0,0 @@
-abstract class Secrets {
-  static String clientSecret = "ADD YOUR KEY HERE";
-  static String encryptionKey = 'ADD YOUR KEY HERE';
-  static String versionOneApiKey = 'ADD YOUR KEY HERE';
-}
